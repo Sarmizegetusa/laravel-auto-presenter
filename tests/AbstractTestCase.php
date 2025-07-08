bce3cbbf9741dc6704b3ff65cd8631005591302e
Lenin Yee <leninyee@gmail.com>
2025-07-08 11:12:00 -0700

    Fix

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -24,7 +24,7 @@ abstract class AbstractTestCase extends AbstractPackageTestCase
      *
      * @return string
      */
-    protected static function getServiceProviderClass()
+    protected static function getServiceProviderClass(): string
     {
         return AutoPresenterServiceProvider::class;
     }
